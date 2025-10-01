2c7317aaba5b43999a93d4270945d8f744ae76e1
Anastasia <alu.anastasia.gubareva@mataro.epiaedu.cat>
2025-10-01 18:22:40 +0200

    Creació readme

---
+++ readme.mk
@@ -0,0 +1,9 @@
+# Pàgina web personal
+
+Aquesta és una pràctica de Sistemes Informàtics.  
+L’objectiu és crear una pàgina web senzilla amb HTML i CSS, i aprendre a gestionar el projecte amb Git i GitHub.
+
+## Contingut
+- Presentació personal
+- Descripció breu
+- Enllaços útils (GitHub, Instagram i email)
